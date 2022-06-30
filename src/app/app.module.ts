55ae63d7bf5313a9abb13132a6d7b29aae08f080
Alisson Lima <amlxd5@gmail.com>
2022-06-30 15:15:50 -0500

    src

---
+++ src/app/app.module.ts
@@ -0,0 +1,65 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AgendaFormComponent } from './components/agenda-form/agenda-form.component';
+import { AgendaListComponent } from './components/agenda-list/agenda-list.component';
+import { AlertaComponent } from './components/alerta/alerta.component';
+import { AtendimentoListComponent } from './components/atendimento-list/atendimento-list.component';
+import { BarraComandosComponent } from './components/barra-comandos/barra-comandos.component';
+import { ConvenioFormComponent } from './components/convenio-form/convenio-form.component';
+import { ConvenioListComponent } from './components/convenio-list/convenio-list.component';
+import { EspecialidadeFormComponent } from './components/especialidade-form/especialidade-form.component';
+import { EspecialidadeListComponent } from './components/especialidade-list/especialidade-list.component';
+import { FiltroAtendimentoComponent } from './components/filtro-atendimento/filtro-atendimento.component';
+import { LoginComponent } from './components/login/login.component';
+import { PacienteFormComponent } from './components/paciente-form/paciente-form.component';
+import { PacienteListComponent } from './components/paciente-list/paciente-list.component';
+import { ProfissionalFormComponent } from './components/profissional-form/profissional-form.component';
+import { ProfissionalListComponent } from './components/profissional-list/profissional-list.component';
+import { UnidadeFormComponent } from './components/unidade-form/unidade-form.component';
+import { UnidadeListComponent } from './components/unidade-list/unidade-list.component';
+import { UsuarioFormComponent } from './components/usuario-form/usuario-form.component';
+import { UsuarioListComponent } from './components/usuario-list/usuario-list.component';
+import { ErroInterceptor } from './interceptors/erro.interceptor';
+import { RequisicaoInterceptor } from './interceptors/requisicao.interceptor';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AgendaListComponent,
+    AgendaFormComponent,
+    AtendimentoListComponent,
+    BarraComandosComponent,
+    AlertaComponent,
+    LoginComponent,
+    UsuarioListComponent,
+    FiltroAtendimentoComponent,
+    ConvenioFormComponent,
+    ConvenioListComponent,
+    EspecialidadeListComponent,
+    PacienteListComponent,
+    ProfissionalListComponent,
+    UnidadeListComponent,
+    UsuarioFormComponent,
+    UnidadeFormComponent,
+    ProfissionalFormComponent,
+    PacienteFormComponent,
+    EspecialidadeFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: ErroInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: RequisicaoInterceptor, multi: true }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: "https://webacademy-alisson.herokuapp.com"
+};
